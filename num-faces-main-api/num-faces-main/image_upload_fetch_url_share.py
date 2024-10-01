d2161ff4663fd0e85f9dadf7c960e1ae4073dfd3
Aparna garg <90500523+aparnagarg04@users.noreply.github.com>
2024-10-01 23:56:56 +0530

    Update image_upload_fetch_url_share.py

--- num-faces-main-api/num-faces-main/image_upload_fetch_url_share.py
+++ num-faces-main-api/num-faces-main/image_upload_fetch_url_share.py
@@ -13,8 +13,7 @@ def create_s3():
     s3 = boto3.resource(
         service_name='s3',
         region_name='ap-south-1',
-        aws_access_key_id='-',
-        aws_secret_access_key='-'
+        
 
     )
     return s3
